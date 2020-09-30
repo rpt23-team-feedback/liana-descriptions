df1dff05a5767ac1dcdcf17e7a14c2992c0a41bc
Max Gektin <max.gektin@gmail.com>
2020-09-30 10:30:52 -0700

    Add encoding; fix nits

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const Bundles = require('../db/models/bundles.model.js');
 const app = express();
 const port = 3663;
 
+app.use(express.urlencoded({
+  extended: true,
+}));
+
 db.sync({ force: false })
   .then(() => {
     console.log('db synced: ', db);
@@ -25,3 +29,25 @@ db.sync({ force: false })
   .catch((err) => {
     console.log('your error: ', err);
   });
+
+  // GET routing
+app.get('/bundleInfo/:bundleId', (req, res) => {
+  const id = req.params.bundleId;
+  return Bundles.findOne({
+    where: {
+      id
+    },
+  })
+  .then((results) => {
+    if (results) {
+      console.log(results.dataValues);
+      const bundle = results.dataValues;
+      return bundle;
+    } else {
+      return null;
+    }
+  })
+  .then((bundle) => {
+    res.send(JSON.stringify(bundle));
+  })
+});
