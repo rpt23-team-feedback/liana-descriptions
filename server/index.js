e1c30d6bfe96d40f11f0de53ae1d483b3f5e609e
Max Gektin <max.gektin@gmail.com>
2020-10-01 11:20:36 -0700

    Add logging; remove unnecessary db call

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const db = require('../db/index.js');
+const morgan = require('morgan');
 const Bundles = require('../db/models/bundles.model.js');
 
 const app = express();
 const port = 3663;
 
+app.use(morgan);
 app.use(express.urlencoded({
   extended: true,
 }));
@@ -15,16 +17,6 @@ db.sync({ force: false })
     app.listen(port, () => {
       console.log(`dockside at port ${port}`);
     })
-  })
-  .then(() => {
-    Bundles.findOne({
-      where: {
-        id: 100,
-      },
-    })
-    .then((results) => {
-      console.log('results: ', results.dataValues);
-    });
   })
   .catch((err) => {
     console.log('your error: ', err);
