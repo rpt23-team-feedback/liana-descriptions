2ad2587d9663b0f6b3624e3feb19c8ce9d554b99
Max Gektin <max.gektin@gmail.com>
2020-10-29 10:36:03 -0700

    Pull down working deploy

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
+const pwd = require('../dbconfig').mysqlPWD;
 
-const db = new Sequelize ('hbdesc', 'root', '', {
+const db = new Sequelize ('hbdesc', 'root', pwd, {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
